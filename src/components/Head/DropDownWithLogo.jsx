84bd4b2c1b6f18344a1a9b0b9218239e351a04c6
hmmasudsarder <hmmasudsarder1@gmail.com>
2024-08-13 11:30:57 +0600

    navbar proble

--- src/components/Head/DropDownWithLogo.jsx
+++ src/components/Head/DropDownWithLogo.jsx
@@ -2,9 +2,10 @@ import user from "../../assets/man.jpg";
 
 const DropDownWithLogo = () => {
     return (
-        <div>
-            <div className="">
-                <img src={user} className="h-20 w-20 object-cover border-4 border-gray-400 rounded-full cur" alt="" />
+        <div className="h-screen bg-gray-200 flex justify-center pt-28">
+            <div className="relative">
+                <img src={user} className="h-20 w-20 object-cover border-4 border-gray-400 rounded-full cursor-pointer" alt="" />
+                <div className="bg-white p-4 w-52 "></div>
             </div>
         </div>
     );
