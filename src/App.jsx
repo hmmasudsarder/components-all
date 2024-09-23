d358ad1ef1f6ca619750db9fdafb958bdb24962f
hmmasudsarder <hmmasudsarder1@gmail.com>
2024-09-23 12:37:51 +0600

    sidebar complated

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
+import { useState } from "react";
 import Dashboard from "./components/Dashboard/Dashboard";
 import DropDown from "./components/Headers/DropDown";
 import DropDownWithLogo from "./components/Headers/DropDownWithLogo";
 import Navbar from "./components/Headers/Navbar";
 import NavbarTwo from "./components/Headers/NavbarTwo";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
+  const [sidebarToggle, setSidebarToggle] = useState(false)
   return (
     <>
       {/* <Navbar/> */}
       {/* <NavbarTwo/> */}
-      <Dashboard />
+      <Sidebar sidebarToggle={sidebarToggle}/>
+      <Dashboard sidebarToggle={sidebarToggle} setSidebarToggle={setSidebarToggle}/>
       {/* <DropDown/> */}
       {/* <DropDownWithLogo/> */}
     </>
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,22 @@
+import { FaHome } from "react-icons/fa";
 
-const Sidebar = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const Sidebar = ({sidebarToggle}) => {
+  return (
+    <div className={`${sidebarToggle ? "hidden" : "block"} w-64 bg-gray-800 fixed h-full px-4 py-2`}>
+      <div className="my-2 mb-4">
+        <h2 className="text-2xl text-white font-bold">Admin Dashboard</h2>
+      </div>
+      <hr /> 
+      <ul className="mt-3 text-white font-bold">
+        <li className="mb-2 rounded hover:shadow hover:bg-blue-500 py-2">
+          <a href="">
+            <FaHome className="inline-block w-6 h-6 mr-2 -mt-2" />
+            Home
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Sidebar;
